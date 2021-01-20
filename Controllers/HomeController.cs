af65e1bf3438670ddeebc59878a937c7808720f7
Cristian Simion <cris_xtreme_53@yahoo.com>
2021-01-20 19:20:02 +0200

    Add shopping view component and carousel

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Polifloris.Data.Interfaces;
+using Polifloris.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Polifloris.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public HomeController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                FeaturedProducts = _productRepository.GetProducts()
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Polifloris.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Polifloris.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Product> FeaturedProducts { get; set; }
+    }
+}
